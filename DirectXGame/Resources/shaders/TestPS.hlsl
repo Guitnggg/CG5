c79cad8517189d80ce3efc35400bd4e55454bd29
LE2C_16_タカキ_ケンゴ <146917575+Guitnggg@users.noreply.github.com>
2025-06-18 11:46:54 +0900

    完成

--- DirectXGame/Resources/shaders/TestPS.hlsl
+++ DirectXGame/Resources/shaders/TestPS.hlsl
@@ -1,11 +1,18 @@
+#include "Test.hlsli"
+
 struct PixelshaderOutPut
 {
     float32_t4 color : SV_TARGET0;
 };
 
-PixelshaderOutPut main()
+PixelshaderOutPut main(VertexShaderOutput input)
 {
     PixelshaderOutPut output;
-    output.color = float32_t4(1.0f, 1.0f, 1.0f, 1.0f);
+    float32_t2 uv = input.texcoord;
+    
+    // https://learn.microsoft.com/ja-jp/windows/win32/direct3dhlsl/dx-graphics-hlsl-componet-math
+    // 位置セット(x,y,z,w)かカラーセット(r,g,b,a)でアクセスできる
+    output.color = float32_t4(uv.x, uv.y, 0.0f, 1.0f);
+    
     return output;
 }
